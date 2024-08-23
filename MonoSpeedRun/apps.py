82cada7f5c716654e6c46d6ac8d82523fbeaaedb
jaba <za_dengi_Da@gmail.com>
2024-08-23 16:32:59 +0300

    очередное изменение имени, по-моему это уже было

--- MonoSpeedRun/apps.py
+++ MonoSpeedRun/apps.py
@@ -1,13 +1,10 @@
 from django.apps import AppConfig
 
-
 class MonoConfig(AppConfig):
+    name = 'MonoSpeedRun'  # Имя пакета, а не путь к классу
 
-    name = 'MonoSpeedRun.apps.MonoConfig.mono'
-    print('ne mongo db, just mono' * 200)
     plugin_app = {
         "url_config": {
-            # ProjectType.LMS p.s. const LMS equal to "lms.djangoapp"
             'lms.djangoapp': {
                 "namespace": 'mono',
                 "regex": '^mono/',
