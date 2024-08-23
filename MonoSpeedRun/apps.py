236b2e0f4efe82f541018591af2b59c672a04aa5
jaba <za_dengi_Da@gmail.com>
2024-08-23 16:22:04 +0300

    da

--- MonoSpeedRun/apps.py
+++ MonoSpeedRun/apps.py
@@ -3,7 +3,7 @@ from django.apps import AppConfig
 
 class MonoConfig(AppConfig):
 
-    name = 'MonoSpeedRun.apps:MonoConfig.mono'
+    name = 'mono'
     print('ne mongo db, just mono' * 200)
     plugin_app = {
         "url_config": {
