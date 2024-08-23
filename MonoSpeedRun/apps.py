0be157faef681a3cbcca0b0c62c2453c33128ac1
jaba <za_dengi_Da@gmail.com>
2024-08-23 16:33:44 +0300

    мнение

--- MonoSpeedRun/apps.py
+++ MonoSpeedRun/apps.py
@@ -2,7 +2,7 @@ from django.apps import AppConfig
 
 class MonoConfig(AppConfig):
     name = 'MonoSpeedRun'  # Имя пакета, а не путь к классу
-
+    print('мнение о кэшировании?' * 100)
     plugin_app = {
         "url_config": {
             'lms.djangoapp': {
